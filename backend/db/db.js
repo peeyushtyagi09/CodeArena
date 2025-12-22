2949b7a540b7a7896bf138277e4d44434cc1d5f5
peeyushtyagi09 <peeyushtyagi82@gmail.com>
2025-12-22 21:59:00 +0530

    some changes in db file

--- backend/db/db.js
+++ backend/db/db.js
@@ -6,8 +6,8 @@ const connectDB = async() => {
         await mongoose.connect(env.MONGODB_URL);
         console.log("✅ Successfully connected to mongodb ✅");
     }catch(err) {
-        console.error("⚠️ Their is a error in { db.js } file in connection of mongodb ⚠️");
+        console.error("⚠️ Their is a error in { db.js } file in connection of mongodb ");
     }
 }
 
-module.exports = { connectDB };
+module.exports = { connectDB }; 
